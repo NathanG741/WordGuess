5994816c36048417ed23cebcd62f29d2f2606b6c
nathan <nathan.guardado@yahoo.com>
2023-11-08 22:20:39 -0500

    v1 :(

--- src/main/java/com/github/zipcodewilmington/WordGuess.java
+++ src/main/java/com/github/zipcodewilmington/WordGuess.java
@@ -1,8 +1,9 @@
 package com.github.zipcodewilmington;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
-
+//Nathan Guardado
 public class WordGuess {
     //This is a "random" class
     public static void main(String[] args) {
@@ -14,17 +15,37 @@ public class WordGuess {
         int randomNumber = rand.nextInt(10);
         String animal = animalArray[randomNumber];
 
+        String[] chars = animal.split("");
+        String[] blanks = new String[chars.length];
+
+        Arrays.fill(blanks, "_");
+
         int tryCount = animal.length();
         while (true) {
-            System.out.println("Guess an animal.");
+            System.out.println(String.join(" ",blanks));
+            System.out.println("Pick a letter");
 
             Scanner scanner = new Scanner(System.in);
             String playerGuess = scanner.nextLine();
             tryCount--;
             if (animal.contains(playerGuess)) {
                 System.out.println("correct guess ");
+                for (int i = 0; i < chars.length; i++){
+                    if (playerGuess.equalsIgnoreCase(chars[i])){
+                        blanks[i] = playerGuess;
+                    }
+                }
+                if (!Arrays.asList(blanks).contains("_") ){
+                    System.out.println("Pog you win");
+                    break;
+                }
+
             }   else
             {
+                if (tryCount == 0) {
+                    System.out.println("Sadge you lose");
+                    break;
+                }
                 System.out.println("incorrect! number of tries remaining: " + tryCount);
             }
         }
